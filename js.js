538459b0fc31b6e52a4b7b67de6fd837c3cfc7d1
Jonathan Keyuk <Jonkero2@gmail.com>
2015-12-04 01:48:17 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,21 +1,46 @@
 //opendoors functions
-
 function myDoor() {
-    document.getElementById("open1").style.width = "0px";
-    document.getElementById("open2").style.width = "0px";
-    document.getElementById("button").style.opacity = "0";
-  }
-//end opendoors functions
-
-  function textColor() {
-    var x = document.getElementsByTagName("span");
-    if (x[0].style.color == "red") {
-      for (i = 0; i < x.length; i++) {
-        x[i].style.color = "white";
-      }
-    } else {
-      for (i = 0; i < x.length; i++) {
-        x[i].style.color = "red";
-      }
+        document.getElementById("open1").style.width = "0px";
+        document.getElementById("open2").style.width = "0px";
+        document.getElementById("button").style.opacity = "0";
     }
-  }
+    //end opendoors functions
+    //NAV BUTTON FUNCTION
+
+function navB() {
+        var hidden = document.getElementsByTagName("li");
+        if (hidden[1].style.display == "block") {
+            hidden[1].style.display = "none";
+        } else {
+            hidden[1].style.display = "block";
+        }
+    }
+    //text color section one change
+
+function sidebarColor() {
+        var x = document.getElementsByTagName("span");
+        if (x[0].style.color == "red") {
+            for (var i = 0; i < x.length; i++) {
+                x[i].style.color = "white";
+            }
+        } else {
+            for (var i = 0; i < x.length; i++) {
+                x[i].style.color = "red";
+            }
+        }
+    }
+    //change button
+
+function buttonColor() {
+        var y = document.getElementsByClassName("button");
+        if (y[0].style.color == "red") {
+            for (var q = 0; q < y.length; q++) {
+                y[q].style.color = "white";
+            }
+        } else {
+            for (var q = 0; q < y.length; q++) {
+                y[q].style.color = "red";
+            }
+        }
+    }
+    //end color section one change
