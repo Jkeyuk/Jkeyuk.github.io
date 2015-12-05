db272e27a1ef0ae00016575db8ee0e03811e91be
Jonathan Keyuk <Jonkero2@gmail.com>
2015-12-05 10:19:52 -0500

    Update js.js

--- js.js
+++ js.js
@@ -4,53 +4,68 @@ var button = document.getElementsByClassName("button");
 button[1].addEventListener('click', buttonColor, false);
 button[1].addEventListener('click', sidebarColor, false);
 button[0].addEventListener('click', navB, false);
+button[2].addEventListener('click', backGround, false);
 //opendoors event handler
 var doors = document.getElementById("button");
 doors.addEventListener('click', myDoor, false);
 //END event handlers
 //opendoors functions
 function myDoor() {
-        document.getElementById("open1").style.width = "0px";
-        document.getElementById("open2").style.width = "0px";
-        document.getElementById("button").style.opacity = "0";
-    }
-    //end opendoors functions
-    //NAV BUTTON FUNCTION
-
+    document.getElementById("open1").style.width = "0px";
+    document.getElementById("open2").style.width = "0px";
+    document.getElementById("button").style.opacity = "0";
+}
+//end opendoors functions
+//NAV BUTTON FUNCTION
 function navB() {
-        var hidden = document.getElementsByTagName("li");
-        if (hidden[1].style.display == "block") {
-            hidden[1].style.display = "none";
-        } else {
-            hidden[1].style.display = "block";
+    var hidden = document.getElementsByTagName("li");
+    if (hidden[1].style.display == "block") {
+        for (var i = 1; i < hidden.length; i++) {
+            hidden[i].style.display = "none";
+        }
+    } else {
+        for (var i = 0; i < hidden.length; i++) {
+            hidden[i].style.display = "block";
         }
     }
-    //text color section one change
-
+}
+//text color section one change
 function sidebarColor() {
-        var x = document.getElementsByTagName("span");
-        if (x[0].style.color == "red") {
-            for (var i = 0; i < x.length; i++) {
-                x[i].style.color = "white";
-            }
-        } else {
-            for (var i = 0; i < x.length; i++) {
-                x[i].style.color = "red";
-            }
+    var x = document.getElementsByTagName("span");
+    if (x[0].style.color == "red") {
+        for (var i = 0; i < x.length; i++) {
+            x[i].style.color = "white";
+        }
+    } else {
+        for (var i = 0; i < x.length; i++) {
+            x[i].style.color = "red";
         }
     }
-    //change button
-
+}
+//change button
 function buttonColor() {
-        var y = document.getElementsByClassName("button");
-        if (y[0].style.color == "red") {
-            for (var q = 0; q < y.length; q++) {
-                y[q].style.color = "white";
-            }
-        } else {
-            for (var q = 0; q < y.length; q++) {
-                y[q].style.color = "red";
-            }
+    var y = document.getElementsByClassName("button");
+    if (y[0].style.color == "red") {
+        for (var q = 0; q < y.length; q++) {
+            y[q].style.color = "white";
+        }
+    } else {
+        for (var q = 0; q < y.length; q++) {
+            y[q].style.color = "red";
         }
     }
-    //end color section one change
+}
+//end color section one change
+//BACKGROUND PICTURE CHANGE FUNCTION
+function backGround() {
+    var x = document.getElementById("section1");
+    var pic1 =
+        'url(https://cloud.githubusercontent.com/assets/15953484/11427347/17c2043a-9430-11e5-9d25-de80cee38858.jpg)';
+    var pic2 =
+        'url(https://cloud.githubusercontent.com/assets/15953484/11606230/d4814672-9ae7-11e5-8cba-0e7504037070.jpg)';
+    if (x.style.backgroundImage == pic2) {
+        x.style.backgroundImage = pic1;
+    } else {
+        x.style.backgroundImage = pic2;
+    }
+}
