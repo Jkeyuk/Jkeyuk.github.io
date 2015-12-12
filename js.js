0e675e154ba1b8eb9b81bf5faa30e13d66a1c54e
Jonathan Keyuk <Jonkero2@gmail.com>
2015-12-12 06:02:30 -0500

    Update js.js

--- js.js
+++ js.js
@@ -5,8 +5,10 @@ navButton.addEventListener('click', showMenu, false);
 function showMenu() {
         if (skillsButton.style.bottom == "0px") {
             skillsButton.style.bottom = "70px";
+            codePen.style.bottom = "70px";
         } else {
             skillsButton.style.bottom = "0px";
+            codePen.style.bottom = "0px";
         }
     }
     //SKILLS BUTTON ******************************************
@@ -35,18 +37,26 @@ function scrollToSkills() {
         }
     }
     skillsButton.style.bottom = "70px";
+    codePen.style.bottom = "70px";
 }
 
 function scrollToTop() {
-    var pos = window.pageYOffset;
-    var id = setInterval(frame, 1);
+        var pos = window.pageYOffset;
+        var id = setInterval(frame, 1);
 
-    function frame() {
-        if (pos <= 0) {
-            clearInterval(id);
-        } else {
-            pos -= 5;
-            window.scrollBy(0, -5);
+        function frame() {
+            if (pos <= 0) {
+                clearInterval(id);
+            } else {
+                pos -= 5;
+                window.scrollBy(0, -5);
+            }
         }
     }
+    //codePen BUTTON ******************************************
+var codePen = document.getElementById("codePen");
+codePen.addEventListener('click', toCodePen, false);
+
+function toCodePen() {
+    window.location.assign("http://codepen.io/Jkeyuk/");
 }
