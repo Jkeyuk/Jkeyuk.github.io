317e4d5a58d588fa9b2f751f84294cdfb21bfbd1
Jonathan Keyuk <Jonkero2@gmail.com>
2015-12-19 02:17:12 -0500

    Update Listmaker.js

--- ListMaker/Listmaker.js
+++ ListMaker/Listmaker.js
@@ -8,61 +8,53 @@ var array = [];
 window.onload = loadSession();
 
 function addAndDisplay() {
-  var output1 = document.getElementById("output1");
-  var input1 = document.getElementById("input1");
-
-  //checks for input***
-  if (input1.value === "") {
-    window.alert("You did not input anything");
-
-    //push value to array***
-  } else {
-    var input1Value = input1.value;
-    window.array.push(input1Value);
-    displayList();
-  }
-  input1.value = "";
-
-  saveSession();
+    var output1 = document.getElementById("output1");
+    var input1 = document.getElementById("input1");
+    //checks for input***
+    if (input1.value === "") {
+        window.alert("You did not input anything");
+        //push value to array***
+    } else {
+        var input1Value = input1.value;
+        window.array.push(input1Value);
+        displayList();
+    }
+    input1.value = "";
+    saveSession();
 }
 
 function saveSession() {
-  //convert array to JSON and save to session
-  var dataAsJson = JSON.stringify(window.array);
-  sessionStorage.savedFile = dataAsJson;
-
+    //convert array to JSON and save to session
+    var dataAsJson = JSON.stringify(window.array);
+    localStorage.savedFile = dataAsJson;
 }
 
 function loadSession() {
-  window.array = JSON.parse(sessionStorage.savedFile);
-
-  displayList();
-
+    window.array = JSON.parse(localStorage.savedFile);
+    displayList();
 }
 
 function deleteList() {
-  var x = window.confirm("ARE YOU SURE YOU WANT TO DELETE?");
-  if (x) {
-    window.array = [];
-    delete sessionStorage.savedFile;
-    output1.innerHTML = "";
-  } else {
-    return;
-  }
-
+    var x = window.confirm("ARE YOU SURE YOU WANT TO DELETE?");
+    if (x) {
+        window.array = [];
+        delete localStorage.savedFile;
+        output1.innerHTML = "";
+    } else {
+        return;
+    }
 }
 
 function displayList() {
-  //Display array list**
-  output1.innerHTML = "";
-  for (i = 0; i < window.array.length; i++) {
-    output1.innerHTML += "<li>" + window.array[i] + "</li> ";
-  }
+    //Display array list**
+    output1.innerHTML = "";
+    for (i = 0; i < window.array.length; i++) {
+        output1.innerHTML += "<li>" + window.array[i] + "</li> ";
+    }
 }
 
 function removeLast() {
-  window.array.pop();
-  saveSession();
-  displayList();
-
+    window.array.pop();
+    saveSession();
+    displayList();
 }
