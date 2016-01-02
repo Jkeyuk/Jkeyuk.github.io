c5c27cff7443bf04377e1eda68de549a4955560a
Jonathan Keyuk <Jonkero2@gmail.com>
2016-01-02 04:17:11 -0500

    Create budgetor.js

---
+++ budgetor/budgetor.js
@@ -0,0 +1,84 @@
+var itemInput = document.getElementById('itemInput');
+var priceInput = document.getElementById('priceInput');
+var addButton = document.getElementById('addButton');
+var output = document.getElementById('output');
+var total = document.getElementById('total');
+//list item class constructer******************************************
+function ListItem(name, price) {
+        this.name = name || 'NoName';
+        this.price = price || 0;
+        this.displayListItem = function() {
+            output.innerHTML += "<div class='outputWrap'>" +
+                "<input type='checkbox' class='checkBox'></input>" +
+                "<p class='listItem'>" + this.name + "</p>" +
+                "<button class='removeButton'>X</button>" +
+                "<p class='itemPrice'>" + '$' + this.price + "</p>" +
+                "</div>";
+        }
+    }
+    //list object********************************
+var list = {
+        listItems: [],
+        addListItem: function() {
+            var itemName = itemInput.value;
+            var itemPrice = priceInput.value;
+            var newListItem = new ListItem(itemName, itemPrice);
+            list.listItems.push(newListItem);
+            itemInput.value = '';
+            priceInput.value = '';
+        },
+        removeListItem: function(index) {
+            if (window.confirm('DELETE ITEM??')) {
+                this.listItems.splice(index, 1)
+            } else {
+                return;
+            }
+        },
+        totalPrice: function() {
+            var total = 0;
+            for (var i = 0; i < this.listItems.length; i++) {
+                total += parseFloat(this.listItems[i].price);
+            }
+            return total.toFixed(2);
+        },
+        displayList: function() {
+            output.innerHTML = '';
+            for (var i = 0; i < this.listItems.length; i++) {
+                list.listItems[i].displayListItem();
+            }
+            total.innerHTML = '$' + list.totalPrice();
+        },
+        addEventsToButtons: function() {
+            removeButton = document.getElementsByClassName('removeButton');
+            checkBox = document.getElementsByClassName('checkBox');
+            outputWrap = document.getElementsByClassName('outputWrap');
+            var length = this.listItems.length;
+            for (var i = 0; i < length; i++) {
+                function inputIndex(index) {
+                    removeButton[i].onclick = function() {
+                        list.removeListItem(index);
+                        list.displayList();
+                        list.addEventsToButtons();
+                    }
+                    checkBox[i].onclick = function() {
+                        if (checkBox[index].checked) {
+                            outputWrap[index].style.background =
+                                'cyan';
+                        } else {
+                            outputWrap[index].style.background =
+                                'transparent';
+                        }
+                    }
+                }
+                inputIndex(i);
+            }
+        }
+    }
+    //add button event***************************
+addButton.onclick = function() {
+        list.addListItem();
+        list.displayList();
+        list.addEventsToButtons();
+    }
+    //dev tools
+    //window.alert(1);
