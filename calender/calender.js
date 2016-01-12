52e2346fc3ffd991b347a6be4f6e2d94d97346a3
Jonathan Keyuk <Jonkero2@gmail.com>
2016-01-12 11:04:52 -0500

    Update calender.js

--- calender/calender.js
+++ calender/calender.js
@@ -1,42 +1,45 @@
 var yearHeader = document.getElementById('yearHeader');
 var day = document.getElementsByClassName('day');
-var monthButton = document.getElementsByClassName('month');
+var dateSelectButton = document.getElementById('dateSelectButton');
 var monthHeader = document.getElementById('monthHeader');
 var dayHeader = document.getElementById('dayHeader');
 var eventList = document.getElementById('eventList');
 var eventInput = document.getElementById('eventInput');
-var addEventBut = document.getElementById('addEventBut');
 var dateInput = document.getElementById('dateInput');
-var eventStorage = [];
+var eventInputWrap = document.getElementById('eventInputWrap');
+var datePicker = document.getElementById('datePicker');
+var listOfMonths = document.getElementById('listOfMonths');
+var eventStorage = [];//Local Storage use
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 //***********objects and constructors********************************
-//event object constructor
-function Event(dateObject, description) {
+function Event(dateObject, description) {//event object constructor
   this.dateObject = dateObject;
   this.description = description;
 }
-//day interface div object*********************
+//Day Object******************************
 var dayInterface = {
-    dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
-      'Friday', 'Saturday'
-    ],
-    events: [],
-    //fill day interface with date and daily events
-    dayFill: function(dateObject) {
-      var dayNames = dayInterface.dayNames;
-      var monthNames = yearInterface.monthNames;
+    events: [],//daily events go here
+    
+    dayFill: function(dateObject) {//sets day header and lists events
       var dayIndex = dateObject.getDay();
       var monthIndex = dateObject.getMonth();
       var date = dateObject.getDate();
+      var year = dateObject.getFullYear();
       var eventsArray = dayInterface.events;
 
-      dayHeader.innerHTML = dayNames[dayIndex] + ' ' + monthNames[monthIndex] + ' ' + date;
-      eventList.innerHTML = "";
-      for (var i = 0; i < eventsArray.length; i++) {
-        eventList.innerHTML += "<li class='eventListItem'>" + eventsArray[i].description + "</li>";
+      dayHeader.innerHTML = dayNames[dayIndex] + ' ' + monthNames[monthIndex] + ' ' + date + ' ' + year;
+      if (eventsArray.length > 0) {
+        eventList.innerHTML = "";
+        for (var i = 0; i < eventsArray.length; i++) {
+          eventList.innerHTML += "<li class='eventListItem'>" + eventsArray[i].description + "</li>";
+        }
+      } else {
+        eventList.innerHTML = "No Events Today";
       }
     },
-    //filter eventstorage and push to daily events
-    filterEvents: function(day, monthIndex, year) {
+   
+    filterEvents: function(day, monthIndex, year) { //filter eventstorage and push to daily events
       var passed = [];
       for (var i = 0; i < eventStorage.length; i++) {
         var TestDay = eventStorage[i].dateObject.getDate();
@@ -47,80 +50,77 @@ var dayInterface = {
         }
       }
       dayInterface.events = passed;
-    },
-    //set add button
-    setButtonEvents: function() {
-      addEventBut.onclick = function() {
-        addEventButtonFunc();
-      }
     }
   }
-  //month interface div object****************************
+//Calender object****************************
 var monthInterface = {
-    //fill month interface grid with dates
-    monthFill: function(dateObject) {
-      var year = dateObject.getFullYear();
-      var monthIndex = dateObject.getMonth();
-      var startOfMonth = new Date(year, monthIndex, 1);
-      var endOfMonth = new Date(year, (monthIndex + 1), 0);
-      var lengthOfMonth = endOfMonth.getDate();
-      var startingDay = startOfMonth.getDay();
-      monthHeader.innerHTML = yearInterface.monthNames[monthIndex];
-      for (var i = 0; i < day.length; i++) {
-        day[i].innerHTML = '';
-      }
-      for (var i = 0; i < lengthOfMonth; i++) {
-        day[i + startingDay].innerHTML = i + 1;
-      }
-    },
-    //set button events for month interface
-    setButtonEvents: function() {
-      var currentYear = yearHeader.innerHTML;
-      var monthIndex = yearInterface.monthNames.indexOf(monthHeader.innerHTML);
-      var startingDay = new Date(currentYear, monthIndex, 1).getDay();
-      var lengthOfMonth = new Date(currentYear, (monthIndex + 1), 0).getDate();
-      for (var i = 0; i < day.length; i++) {
-        day[i].onclick = function() {};
-      }
-      for (var i = 0; i < lengthOfMonth; i++) {
-        day[i + startingDay].onclick = function() {
-          dayButtonFunc(this);
-        }
-      }
-    }
-  }
-  //year interface div object**************************************
-var yearInterface = {
-    monthNames: ['January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November',
-      'December'
-    ],
-    //set year interface header 
-    setCurretYear: function(dateObject) {
-      yearHeader.innerHTML = dateObject.getFullYear();
-    },
-    //set year interface events to buttons
-    setButtonEvents: function() {
-      for (var i = 0; i < monthButton.length; i++) {
-        monthButton[i].onclick = function() {
-          monthButtonFunc(this);
-        }
-      }
-    }
-  }
-  //******************Button Functions*********************************
+  
+  monthFill: function(dateObject) {//fill month interface grid with dates
+    var year = dateObject.getFullYear();
+    var monthIndex = dateObject.getMonth();
+    var startOfMonth = new Date(year, monthIndex, 1);
+    var endOfMonth = new Date(year, (monthIndex + 1), 0);
+    var lengthOfMonth = endOfMonth.getDate();
+    var startingDay = startOfMonth.getDay();
 
-function monthButtonFunc(button) {
+    yearHeader.innerHTML = dateObject.getFullYear();
+    monthHeader.innerHTML = monthNames[monthIndex];
+    for (var i = 0; i < day.length; i++) {
+      day[i].innerHTML = '';
+      day[i].style.background = 'white';
+    }
+    for (var i = 0; i < lengthOfMonth; i++) {
+      day[i + startingDay].innerHTML = i + 1;
+    }
+    addColorToEventDays(monthIndex, year, startingDay); //styling
+  }
+}
+
+//******************Button Event Listner************************************
+function buttonEvents() {
   var currentYear = yearHeader.innerHTML;
-  var monthIndex = yearInterface.monthNames.indexOf(button.innerHTML);
-  var dateObject = new Date(currentYear, monthIndex, 1);
+  var monthIndex = monthNames.indexOf(monthHeader.innerHTML);
+  var startingDay = new Date(currentYear, monthIndex, 1).getDay();
+  var lengthOfMonth = new Date(currentYear, (monthIndex + 1), 0).getDate();
+
+  addEventBut.onclick = function() {
+    addEventButtonFunc();
+  }
+  eventInputSliderButton.onclick = function() {
+    eventInputSliderButtonFunc();
+  }
+  dayHeader.onclick = function() {
+    dayHeaderButtonFunc();
+  }
+  for (var i = 0; i < day.length; i++) {
+    day[i].onclick = function() {};
+  }
+  for (var i = 0; i < lengthOfMonth; i++) {
+    day[i + startingDay].onclick = function() {
+      dayButtonFunc(this);
+    }
+  }
+  dateSelectButton.onclick = function() {
+    dateSelectButtonfunc();
+    dateHeaderWrapFunc();
+  }
+  dateHeaderWrap.onclick = function() {
+    dateHeaderWrapFunc();
+  }
+}
+//******************Button Functions*********************************
+function dateSelectButtonfunc() { 
+  var today = new Date();
+  var year = pickYear.value || today.getFullYear();
+  var monthIndex = monthNames.indexOf(pickMonth.value);
+  var dateObject = new Date(year, monthIndex, 1);
   monthInterface.monthFill(dateObject);
-  monthInterface.setButtonEvents();
+  buttonEvents();
 }
 
 function dayButtonFunc(button) {
   var currentYear = yearHeader.innerHTML;
-  var monthIndex = yearInterface.monthNames.indexOf(monthHeader.innerHTML);
+  var monthIndex = monthNames.indexOf(monthHeader.innerHTML);
   var dateObject = new Date(currentYear, monthIndex, button.innerHTML);
   dayInterface.filterEvents(button.innerHTML, monthIndex, currentYear);
   dayInterface.dayFill(dateObject);
@@ -139,8 +139,33 @@ function addEventButtonFunc() {
   localStorage.setItem("jk1", jasonfired);
   dayInterface.filterEvents(dayIndex, monthIndex, year);
   dayInterface.dayFill(dateObject);
+  monthInterface.monthFill(dateObject);
   dateInput.value = '';
   eventInput.value = '';
+}
+
+function eventInputSliderButtonFunc() {
+  if (eventInputWrap.style.height == '80px') {
+    eventInputWrap.style.height = '0px';
+  } else {
+    eventInputWrap.style.height = '80px';
+  }
+}
+
+function dayHeaderButtonFunc() {
+  if (datePicker.style.display == 'block') {
+    datePicker.style.display = 'none';
+  } else {
+    datePicker.style.display = 'block'
+  }
+}
+
+function dateHeaderWrapFunc() {
+  if (datePickerDropDown.style.display == 'block') {
+    datePickerDropDown.style.display = 'none';
+  } else {
+    datePickerDropDown.style.display = 'block'
+  }
 }
 //***************************************************
 //onload
@@ -150,30 +175,44 @@ function initialize() {
   var dayIndex = today.getDate();
   var monthIndex = today.getMonth();
   var year = today.getFullYear();
-  yearInterface.setCurretYear(today);
-  yearInterface.setButtonEvents();
   monthInterface.monthFill(today);
-  monthInterface.setButtonEvents();
   dayInterface.filterEvents(dayIndex, monthIndex, year);
   dayInterface.dayFill(today);
-  dayInterface.setButtonEvents();
+  buttonEvents();
+  pickYear.innerHTML = ''; //set up date picker inputs
+  for (var i = 0; i < 10; i++) {
+    pickYear.innerHTML += '<option value="' + (2016 + i) + '">' + (2016 + i) + '</option>';
+  }
 }
 
 function loadEvents() {
-  var parsedList = JSON.parse(localStorage.getItem('jk1')) || [];  
+  var parsedList = JSON.parse(localStorage.getItem('jk1')) || []; //load localstorage
   for (var i = 0; i < parsedList.length; i++) {
     var dateObject = new Date(Date.parse(parsedList[i].dateObject));
     var description = parsedList[i].description;
     var newEvent = new Event(dateObject, description);
     eventStorage.push(newEvent);
   }
-  
-
 }
 window.onload = function() {
     loadEvents();
-    initialize();   
+    initialize();
   }
-  //***************************************************
-  //localStorage.clear();
-  //window.alert(parsedList[0].description);
+  //*******************Styling and Animations********************************
+
+function addColorToEventDays(monthIndex, year, startingDay) {
+  var passed = [];
+  for (var i = 0; i < eventStorage.length; i++) {
+    var TestMonth = eventStorage[i].dateObject.getMonth();
+    var TestYear = eventStorage[i].dateObject.getFullYear();
+
+    if (TestMonth === monthIndex && TestYear === year) {
+      passed.push(eventStorage[i].dateObject.getDate());
+    }
+  }
+  for (var i = 0; i < passed.length; i++) {
+    day[(passed[i] + startingDay) - 1].style.background = '#00ffff';
+  }
+}
+
+//window.alert(parsedList[0].description);
