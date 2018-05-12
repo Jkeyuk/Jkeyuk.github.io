a41334292e9d34014318ace9c8e674ba4e40271c
Jonathan Keyuk <Jonkero2@gmail.com>
2018-05-12 08:45:15 -0400

    Revert "MobileAdded"
    This reverts commit bdf2b4c3fd41ebc066f2e0e2b374ae60433a25e9.

--- MeteorMaps/detect.js
+++
@@ -1,6 +0,0 @@
-/**
- * Redirect small screens
- */
-if (screen.width <= 1024) {
-    window.location.replace("./mobile/mobile.html");
-}
--- MeteorMaps/index.js
+++ MeteorMaps/index.js
@@ -28,9 +28,7 @@ function setMarker(lat, long, popUpString, map) {
 	L.marker([lat, long]).addTo(map).bindPopup(popUpString).addTo(markerGroup);
 }
 
-/**
- * Load Table and set Events when document finished loading
- */
+//Load Table and set Events when document finished loading
 $(document).ready(function () {
 	/**
 	 * Initializes table with data from meteorData.js
