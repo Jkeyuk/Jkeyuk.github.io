f0857ff94d3602459730f9aeecce5c7b43dfb60d
Jonathan Keyuk <Jonkero2@gmail.com>
2018-05-17 10:04:12 -0400

    jsCodeCleaning
    Redesigned js code to be cleaner

--- budgetor/budgetor.js
+++ budgetor/budgetor.js
@@ -1,107 +1,107 @@
-var itemInput = document.getElementById('itemInput');
-var priceInput = document.getElementById('priceInput');
-var addButton = document.getElementById('addButton');
+// var to hold list object
+var listObject;
 
-var output = document.getElementById('output');
-var total = document.getElementById('total');
+/**
+ * list item class
+ */
+class listItem {
+  constructor(name, price) {
+    this.name = name || 'NoName';
+    this.price = price || 0;
+  }
+}
 
-//list item class constructer******************************************
-function ListItem(name, price) {
-  this.name = name || 'NoName';
-  this.price = price || 0;
+/**
+ * list class
+ */
+class list {
+  constructor(listItems) {
+    this.listItems = listItems || [];
+  }
 
-  this.displayListItem = function() {
+  // adds a list item with a given name and price
+  addListItem(itemName, itemPrice) {
+    this.listItems.push(new listItem(itemName, itemPrice));
+  }
+
+  // removes a list item at a given index
+  removeListItem(index) {
+    this.listItems.splice(index, 1);
+  }
+
+  // saves the list to local storage
+  saveList() {
+    localStorage.setItem("listMaker", JSON.stringify(this));
+  }
+
+  // returns the total price of the list items
+  totalPrice() {
+    var total = 0;
+    for (var i = 0; i < this.listItems.length; i++) {
+      total += parseFloat(this.listItems[i].price);
+    }
+    return total.toFixed(2);
+  }
+}
+
+/**
+ * builds the html for each given list item and displays
+ * them on the page
+ * @param {Array} listItems list items to build
+ */
+function buildList(listItems) {
+  var output = document.getElementById('output');
+  var total = document.getElementById('total');
+  output.innerHTML = '';
+  for (var i = 0; i < listItems.length; i++) {
     output.innerHTML += "<div class='outputWrap'>" +
       "<input type='checkbox' class='checkBox'></input>" +
-      "<p class='listItem'>" + this.name + "</p>" +
-      "<button class='removeButton'>X</button>" +
-      "<p class='itemPrice'>" + '$' + this.price + "</p>" +
+      "<p class='listItem'>" + listItems[i].name + "</p>" +
+      "<button class='removeButton' onclick='removeItem(" + i + ")'>X</button>" +
+      "<p class='itemPrice'>" + '$' + listItems[i].price + "</p>" +
       "</div>";
   }
+  total.innerHTML = '$' + listObject.totalPrice();
 }
-//list object********************************
-var list = {
-    listItems: [],
 
-    addListItem: function() {
-      var itemName = itemInput.value;
-      var itemPrice = priceInput.value;
-      var newListItem = new ListItem(itemName, itemPrice);
-      list.listItems.push(newListItem);
-      itemInput.value = '';
-      priceInput.value = '';
-    },
-    removeListItem: function(index) {
-      if (window.confirm('DELETE ITEM??')) {
-        this.listItems.splice(index, 1);
-      } else {
-        return;
-      }
-    },
-    saveList: function() {
-      var jsonfiedList = JSON.stringify(this);
-      localStorage.setItem("listMaker", jsonfiedList);
-    },
-    loadList: function() {
-      var parsedList = JSON.parse(localStorage.getItem('listMaker'));
-
-      for (var i = 0; i < parsedList.listItems.length; i++) {
-        var newListItem = new ListItem(parsedList.listItems[i].name,
-          parsedList.listItems[i].price);
-        list.listItems.push(newListItem);
-      }
-
-    },
-    totalPrice: function() {
-      var total = 0;
-      for (var i = 0; i < this.listItems.length; i++) {
-        total += parseFloat(this.listItems[i].price);
-      }
-      return total.toFixed(2);
-    },
-    displayList: function() {
-      output.innerHTML = '';
-      for (var i = 0; i < this.listItems.length; i++) {
-        list.listItems[i].displayListItem();
-      }
-      total.innerHTML = '$' + list.totalPrice();
-    },
-    addEventsToButtons: function() {
-      removeButton = document.getElementsByClassName('removeButton');
-      checkBox = document.getElementsByClassName('checkBox');
-      outputWrap = document.getElementsByClassName('outputWrap');
-      var length = this.listItems.length;
-
-      for (var i = 0; i < length; i++) {
-
-        function inputIndex(index) {
-          removeButton[i].onclick = function() {
-            list.removeListItem(index);
-            list.displayList();
-            list.addEventsToButtons();
-            list.saveList();
-          }
-          checkBox[i].onclick = function() {
-            if (checkBox[index].checked) {
-              outputWrap[index].style.background = 'cyan';
-            } else {
-              outputWrap[index].style.background = 'transparent';
-            }
-          }
-        }
-        inputIndex(i);
-      }
-    }
+/**
+ * removes a list item at a given index, saves the list and
+ * rebuilds the list to display
+ * @param {number} index index of the list item to remove
+ */
+function removeItem(index) {
+  if (window.confirm('DELETE ITEM??')) {
+    listObject.removeListItem(index);
+    listObject.saveList();
+    buildList(listObject.listItems);
+  } else {
+    return;
   }
-  //add button event***************************
-addButton.onclick = function() {
-  list.addListItem();
-  list.displayList();
-  list.addEventsToButtons();
-  list.saveList();
 }
-window.onload = function() {
-  list.loadList();
-  list.displayList();
-  list.addEventsToButtons();
+
+/**
+ * Adds a new list item to the list from the input boxes. rebuilds the
+ * list to display, and saves the list.
+ */
+function addListItem() {
+  var itemInput = document.getElementById('itemInput');
+  var priceInput = document.getElementById('priceInput');
+  listObject.addListItem(itemInput.value, priceInput.value);
+  itemInput.value = '';
+  priceInput.value = '';
+  buildList(listObject.listItems);
+  listObject.saveList();
+}
+
+/**
+ * on load: loads the list from local storage or initializes an empty list
+ */
+window.onload = function () {
+  var parsedList = JSON.parse(localStorage.getItem('listMaker'));
+  if (parsedList) {
+    listObject = new list(parsedList.listItems);
+    buildList(listObject.listItems);
+  } else {
+    listObject = new list();
+  }
 }
